2b2fbb308e755feeef869c5e224659d60f5bcb91
Thiago Mota <thiagomotape@hotmail.com.br>
2025-03-12 21:29:21 -0300

    feat: fiz o filtro por categoria funcionar

--- capputeeno/src/hooks/useProducts.tsx
+++ capputeeno/src/hooks/useProducts.tsx
@@ -1,30 +1,43 @@
 import { ProductsFetchResponse } from "@/types/products-response";
 import { useQuery } from "@tanstack/react-query";
 import axios, { AxiosPromise } from "axios";
+import { useFilter } from "./useFilter";
+import { FilterType } from "@/types/filter-types";
+import { getCategoryByType } from "@/utils/get-category-by-type";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
 
-const fetcher = (): AxiosPromise<ProductsFetchResponse> => {
-    return axios.post(
-        API_URL,
-        {
-            query:
-                `query {
-                    allProducts {
-                        id
-                        name
-                        price_in_cents
-                        image_url
-                    }
-                }`
-        }
-    )
+const fetcher = (query: string): AxiosPromise<ProductsFetchResponse> => {
+    return axios.post(API_URL, { query })
+}
+
+const mountQuery = (type: FilterType) => {
+    if(type === FilterType.ALL) return `query {
+                                            allProducts {
+                                                id
+                                                name
+                                                price_in_cents
+                                                image_url
+                                                category
+                                            }
+                                        }`
+    return `query {
+                allProducts(filter: { category: "${getCategoryByType(type)}" }) {
+                    id
+                    name
+                    price_in_cents
+                    image_url
+                    category
+                }
+            }`
 }
 
 export function useProducts() {
+    const { type } = useFilter()
+    const query = mountQuery(type)
     const { data } = useQuery({
-        queryFn: fetcher,
-        queryKey: ['products']
+        queryFn: () => fetcher(query),
+        queryKey: ['products', type]
     })
 
     return {
--- capputeeno/src/types/product.ts
+++ capputeeno/src/types/product.ts
@@ -2,5 +2,6 @@ export interface Product {
     name: string,
     price_in_cents: number,
     id: string,
-    image_url: string
+    image_url: string,
+    category: string
 }
