b23d74357e401d668c21250859ca7e962dde163e
Thiago Mota <thiagomotape@hotmail.com.br>
2025-03-12 21:08:14 -0300

    feat: mostrando a lista de produtos na tela

--- capputeeno/src/components/products-list.tsx
+++ capputeeno/src/components/products-list.tsx
@@ -1,12 +1,27 @@
 "use client"
 import { useProducts } from "@/hooks/useProducts"
+import ProductCard from "./product-card";
+import { styled } from "styled-components";
 
-interface ProductListProps {}
+const ListContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 256px);
+    grid-gap: 32px;
+    max-width: 100%;
+    margin-top: 32px;
+`
 
-export default function ProductList(props : ProductListProps){
+export default function ProductList(){
     const { data } = useProducts();
     console.log(data)
     return(
-        <></>
+        <ListContainer>
+            {data?.map(product => <ProductCard
+                key={product.id}
+                image={product.image_url}
+                title={product.name}
+                price={product.price_in_cents}
+            />)}
+        </ListContainer>
     )
 }
