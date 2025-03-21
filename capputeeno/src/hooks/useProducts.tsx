10948f299e4bd35aeda82b7ed33b05a3379aa183
Thiago Mota <thiagomotape@hotmail.com.br>
2025-03-20 22:36:23 -0300

    fix: configurei o tempo do cache para as requisições

--- capputeeno/src/hooks/useProducts.tsx
+++ capputeeno/src/hooks/useProducts.tsx
@@ -17,7 +17,8 @@ export function useProducts() {
     const { data } = useQuery({
         queryFn: () => fetcher(query),
         queryKey: ['products', type, priority],
-        enabled: query !== ""
+        enabled: query !== "",
+        staleTime: 1000*60*1
     })
     const products = data?.data?.data?.allProducts 
     const filteredProducts = products?.filter(product => product.name.toLowerCase().includes(searchDeferred.toLowerCase()))
