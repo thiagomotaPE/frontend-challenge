a4da09e786041b271f33335d72e36a2db4fbba28
Thiago Mota <thiagomotape@hotmail.com.br>
2025-03-22 20:59:04 -0300

    feat: adicionei navegação para o carrinho

--- capputeeno/src/components/cart-control.tsx
+++ capputeeno/src/components/cart-control.tsx
@@ -2,9 +2,13 @@
 import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { CartIcon } from "./icons/cart-icon";
 import { styled } from "styled-components";
+import { useRouter } from "next/navigation";
 
-const Container = styled.div`
+const Container = styled.button`
     position: relative;
+    border: none;
+    background: transparent;
+    cursor: pointer;
 `
 
 const CartCount = styled.span`
@@ -21,10 +25,15 @@ const CartCount = styled.span`
 `
 
 export function CartControl() {
+    const router = useRouter()
     const { value } = useLocalStorage('cart-items', [])
+
+    const handleNavigateToCart = () => {
+        router.push("/cart")
+    }
     
     return (
-        <Container>
+        <Container onClick={handleNavigateToCart}>
             <CartIcon/>
             {value.length > 0 && <CartCount>{value.length}</CartCount>}
         </Container>
